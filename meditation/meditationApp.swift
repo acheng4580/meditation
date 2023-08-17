2b7723ba3f64ea0a126d3a2d3332f08fc053a766
Audrey Cheng <audreyellacheng@icloud.com>
2023-08-17 10:44:58 -0400

    Initial Commit

---
+++ meditation/meditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  meditationApp.swift
+//  meditation
+//
+//  Created by Audrey Cheng on 2023-08-17.
+//
+
+import SwiftUI
+
+@main
+struct meditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
